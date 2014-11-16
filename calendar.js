1a43b06ad350ed33a9d2e328a5225c57451a51e9
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-16 11:52:30 -0800

    Load number of events in Nov 2014 from Google Calendar

---
+++ calendar.js
@@ -0,0 +1,82 @@
+// From http://googleappsdeveloper.blogspot.com/2011/12/using-new-js-library-to-unlock-power-of.html
+
+var clientId = '366846958680-246mghtr1mjlj3s20gil8j1nd95nacll.apps.googleusercontent.com';
+var apiKey = 'YZvmaxmah0WrfrqxkDK2TAly';
+var scopes = 'https://www.googleapis.com/auth/calendar.readonly';
+
+function handleClientLoad() {
+  gapi.client.setApiKey(apiKey);
+  window.setTimeout(checkAuth,1);
+  checkAuth();
+}
+
+function checkAuth() {
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+      handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+  var authorizeButton = document.getElementById('authorize-button');
+  if (authResult) {
+    authorizeButton.style.visibility = 'hidden';
+    makeApiCall();
+  } else {
+    authorizeButton.style.visibility = '';
+    authorizeButton.onclick = handleAuthClick;
+   }
+}
+
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+      {client_id: clientId, scope: scopes, immediate: false},
+      handleAuthResult);
+  return false;
+}
+
+var loadedData = null;
+
+function makeApiCall() {
+  gapi.client.load('calendar', 'v3', function() {
+    var request = gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'singleEvents': true,
+      // TODO: use today's date
+      "timeMin": "2014-11-01T00:00:00-08:00",
+      "timeMax": "2014-12-01T00:00:00-08:00",
+    });
+    
+    request.execute(function(resp) {
+      console.log("Loaded calendar data", resp);
+      loadedData = processCalendar(resp);
+      if (waitingCallback) {
+        waitingCallback(loadedData);
+      }
+    });
+  });
+}
+
+function processCalendar(resp) {
+  var result = [];
+  for (var i = 0; i < 31; i++) {
+    result.push(0);
+  }
+  var items = resp.items;
+  items.forEach(function(item) {
+    if (!item.start.dateTime) return;
+    var yearMonth = item.start.dateTime.slice(0,7);
+    if (yearMonth != "2014-11") return; // TODO: use today's date
+    var day = parseInt(item.start.dateTime.slice(8,10));
+    result[day] += 1;
+  });
+  console.log("Calendar results", result)
+  return result;
+}
+
+
+function loadCalendar(callback) {
+  if (loadedData) {
+    callback(loadedData);
+  } else {
+    waitingCallback = callback;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+var data = [10, 5];
+
 function setup() {
-  // put setup code here
+  createCanvas(800, 600);
+  loadCalendar(function(d) {
+    data = d;
+    clear();
+  });
 }
 
 function draw() {
-  // put drawing code here
+  var i = 0;
+  var x, y;
+  if (!data.forEach) return;
+  data.forEach(function(n) {
+    var scale = n/6;
+    y = Math.floor(i/7) * 100;
+    x = (i%7) * 100;
+    ellipse(x + 50, y + 50, 80*scale, 80*scale);
+    i += 1;
+  });
 }
