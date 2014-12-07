ac474f71a939df24a09a88f84bab59d846f37a5e
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-12-07 15:46:10 -0800

    Added real tiles

--- 1event.js
+++ 1event.js
@@ -1,14 +1,16 @@
-var data = [1, 2, 3, 4, 5];
 var gutter = 10;
 
 function setup() {
-  createCanvas(160*7 + gutter*6, 2000);
+  createCanvas(2000 + gutter, 160);
   angleMode(DEGREES);
-  loadCalendar(function(d) {
-    data = d;
-    clear();
-  });
 }
+
+function drawBackground() {
+  noStroke();
+  fill(240, 240, 240);
+  rect(0, 0, 160, 160);
+}
+
 
 function tile1() {
   stroke(0, 0, 0);
@@ -42,23 +44,23 @@ function tile5() {
   tile4();
 }
 
+
 function draw() {
-  var i = 0;
+  drawBackground();
+ 
   
   translate(80, 80);
+  tile1();
   
-  if (!data.forEach) return;
-  data.forEach(function(n) {
-    if (n == 1) tile1();
-    if (n == 2) tile2();
-    if (n == 3) tile3();
-    if (n == 4) tile4();
-    if (n >= 5) tile5();
-    i += 1;
-    if (i % 7 == 0) {
-      translate((160+gutter)*-6, 160);
-    } else {
-      translate(160 + gutter, 0);
-    }
-  });
+  translate(160 + gutter, 0);
+  tile2();
+  
+  translate(160 + gutter, 0);
+  tile3();
+  
+  translate(160 + gutter, 0);
+  tile4();
+  
+  translate(160 + gutter, 0);
+  tile5();
 }
