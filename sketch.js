8df47a8ac728da74f6103e6e56380017e6b9164b
Your Name HereChennech <email addresschen@on-pixel.com>
2015-01-04 11:48:17 -0800

    Add calendar layout

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var curve;
 var smooth;
 
 function setup() {
-  createCanvas(160*8 + gutter*7, 2000);
+  createCanvas(1280, 2000);
   angleMode(DEGREES);
   loadCalendar(function(d) {
     data = d;
@@ -56,11 +56,35 @@ function tile6() {
   
 }
 
+function drawCalendarFrame(title) {
+  noStroke();
+  fill(241,241,241);
+  rect(40,320,1200,890);
+  
+  fill(0,0,0);
+  textSize(92);
+  text(title, 40, 120);
+  
+  // days of the week
+  textAlign(CENTER);
+  textSize(20);
+  text("SUNDAY",    40+gutter+80, 300);
+  text("MONDAY",    40+gutter+80 + (gutter+160)*1, 300);
+  text("TUESDAY",   40+gutter+80 + (gutter+160)*2, 300);
+  text("WEDNESDAY", 40+gutter+80 + (gutter+160)*3, 300);
+  text("THURSDAY",  40+gutter+80 + (gutter+160)*4, 300);
+  text("FRIDAY",    40+gutter+80 + (gutter+160)*5, 300);
+  text("SATURDAY",  40+gutter+80 + (gutter+160)*6, 300);
+}
+
 function draw() {
   if (hasDrawn) return;
   
+  drawCalendarFrame("MARCH 2015");
+  
   var i = 0;
   
+  translate(40+gutter, 320 + gutter);
   translate(80, 80);
   
   if (!data.forEach) return;
@@ -72,7 +96,7 @@ function draw() {
     if (n == 5) tile5();
     if (n >= 6) tile6();
     i += 1;
-    if (i % 6 == 0) {
+    if (i % 7 == 0) {
       translate((160+gutter)*-6, 160);
     } else {
       translate(160 + gutter, 0);
