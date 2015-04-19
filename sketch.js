98ccf3b977adaa3eb2f4f3071961ff8f9d3b9ceb
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-04-19 15:17:28 -0700

    Extract variables for colors and position of the grid

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 var data = [1, 2, 3, 4, 1,2,4, 1, 5, 3, 4, 2,6,5,4,1, 5, 3, 1,2,1,4,3, 2, 3, 5,4, 5,6,1,4];
+
+var gridLeft = 40;
+var gridTop = 100;
 var gutter = 10;
+
+var colors;
+
 var hasDrawn = false;
 var curve;
 var smooth;
-
-
 
 var lineTileset = {
   tile1: function() {
@@ -83,6 +87,13 @@ var curvedTileset = {
 var tileset = curvedTileset;
 
 function setup() {
+  colors = {
+    background: color(241, 241, 241),
+    highlight: color(239, 221, 204),
+    cell: color(255, 255, 255),
+    line: color(0, 0, 0)
+  };
+  
   createCanvas(1280, 2000);
   angleMode(DEGREES);
   loadCalendar(function(d) {
@@ -91,47 +102,40 @@ function setup() {
     clear();
   });
   curve = random(200) - 100;
-  smooth = random(200) - 100;
-  
-  }
-  
-  
-
-
+  smooth = random(200) - 100;  
+}
 
 function drawCalendarFrame(title) {
   noStroke();
-  fill(241,241,241);
-  rect(40,320,1200,890);
+  fill(colors.background);
+  rect(gridLeft,gridTop + 20,1200,890);
   
-  fill(0,0,0);
+  fill(colors.line);
   textSize(92);
-  text(title, 40, 120);
+  text(title, gridLeft, gridTop-180);
   
   // days of the week
   textAlign(CENTER);
   textSize(20);
-  text("SUNDAY",    40+gutter+80, 300);
-  text("MONDAY",    40+gutter+80 + (gutter+160)*1, 300);
-  text("TUESDAY",   40+gutter+80 + (gutter+160)*2, 300);
-  text("WEDNESDAY", 40+gutter+80 + (gutter+160)*3, 300);
-  text("THURSDAY",  40+gutter+80 + (gutter+160)*4, 300);
-  text("FRIDAY",    40+gutter+80 + (gutter+160)*5, 300);
-  text("SATURDAY",  40+gutter+80 + (gutter+160)*6, 300);
+  text("SUNDAY",    gridLeft+gutter+80, gridTop);
+  text("MONDAY",    gridLeft+gutter+80 + (gutter+160)*1, gridTop);
+  text("TUESDAY",   gridLeft+gutter+80 + (gutter+160)*2, gridTop);
+  text("WEDNESDAY", gridLeft+gutter+80 + (gutter+160)*3, gridTop);
+  text("THURSDAY",  gridLeft+gutter+80 + (gutter+160)*4, gridTop);
+  text("FRIDAY",    gridLeft+gutter+80 + (gutter+160)*5, gridTop);
+  text("SATURDAY",  gridLeft+gutter+80 + (gutter+160)*6, gridTop);
   
   push();
-  stroke(0,0,0);
-  translate(1100, 80);
+  stroke(colors.line);
+  translate(gridLeft-40+1100, gridTop-300+80);
   scale(0.7, 0.7);
   tileset.logo();  
   pop();
-    
-  
 }
 
 function whiteGrid() {
 	noStroke();
-	fill(255,255,255);
+	fill(colors.cell);
 	rect(-80,-80,160,160);
 }
 
@@ -141,26 +145,20 @@ function draw() {
   drawCalendarFrame("March 2015");
   	  
   var i = 0; 
-  translate(40+gutter, 320 + gutter);
+  translate(gridLeft+gutter, gridTop + 20 + gutter);
   translate(80, 80);
    
   if (!data.forEach) return;
   data.forEach(function(n) {
-    
-      
     i += 1;
     if (i % 7 == 0) {
      translate((160+gutter)*-6, 160+gutter);
      whiteGrid();
-       stroke(0,0,0);
-     
-     
+     stroke(colors.line);
     } else {
       translate(160 + gutter, 0);
       whiteGrid();
-        stroke(0,0,0);
-      
-      
+      stroke(colors.line);
     }
     
     if (n == 1) tileset.tile1();
